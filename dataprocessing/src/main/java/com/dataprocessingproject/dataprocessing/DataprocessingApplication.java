2b7424070e09b5e2ef7467039b0cfe8b06b5b40a
Yanniek <yanniek.wielage@student.nhlstenden.com>
2021-03-30 15:42:53 +0200

    Initial Code push

---
+++ dataprocessing/src/main/java/com/dataprocessingproject/dataprocessing/DataprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.dataprocessingproject.dataprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataprocessingApplication.class, args);
+	}
+
+}
---
+++ dataprocessing/src/test/java/com/dataprocessingproject/dataprocessing/DataprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataprocessingproject.dataprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
